3e1710d438714c86024864ca848568c2b4091153
MyLab6 <asou1121@gmail.com>
2021-12-18 12:02:36 -0500

    should be it

--- setup_midi.py
+++ setup_midi.py
@@ -1,22 +1,27 @@
 import subprocess
 import json 
+import os 
 
-def copy_file(midi_file):
-    file_to_cp = 'files_to_copy/' +  midi_file['repo_location'] 
-
-    shutil.copy( file_to_cp, midi_file['file_sys_location'] )
-    if( file_instance['make_exec']):
-        exec('sudo chmod +x ' +  file_instance['file_sys_location'])
-
-
+# first install bluez 
+run_commands("setup_bt_commands.txt")
 files = json.loads("file_locations.json")
 for local_file in files: 
     copy_file(local_file)
 # run various commands 
+run_commands("setup_midi_commands.txt")
 
-def set_up_midi_service():
-  
-def set_up_bluetooth():
-
-def run_command(command):
-    subprocess.run(command, shell=True, check=True)
+def run_commands(file_location):
+    final_path = get_local_file_path( file_location)    
+    file1 = open(final_path, 'r')
+    Lines = file1.readlines()
+    for command in Lines:
+        subprocess.run(command, shell=True, check=True)
+def copy_file(midi_file):    
+    almost_file = os.path.join( 'files_to_copy' ,midi_file['repo_location'])
+    file_to_cp = get_local_file_path(almost_file) 
+    shutil.copy( file_to_cp, midi_file['file_sys_location'] )
+    if( file_instance['make_exec']):
+        exec('sudo chmod +x ' +  file_instance['file_sys_location'])
+def get_local_file_path(sub_path):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(dir_path,sub_path)
