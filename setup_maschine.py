f0edceeb4577095ac5cdcefb2eb1b261823c2172
MyLab6 <asou1121@gmail.com>
2022-01-01 02:06:36 -0500

    more

--- setup_maschine.py
+++ setup_maschine.py
@@ -3,20 +3,31 @@ import json
 import os 
 import shutil
 
+
+def get_hdraw(regex):
+    regexResult = re.search(",.*hidraw.*:",regex)
+    return regexResult.group(0).replace(",","").replace(":","")
+
 def set_up_maschine():
-    subprocess.run("curl https://sh.rustup.rs -sSf | bash -s -- -y")
-    subprocess.run("source $HOME/.cargo/env")
+    try:
+        subprocess.run("curl https://sh.rustup.rs -sSf | bash -s -- -y")
+        subprocess.run("source $HOME/.cargo/env")
+    except:
+        pass
+    
     results = subprocess.check_output("dmesg", universal_newlines=True).split('\n')
+    hdraw = ""
     for device in results:
-        if "Native Instruments".lower() in device.lower():
+        if "Native Instruments".lower() in device.lower() and "hidraw" in device.lower():
             print(device)
+            hdraw = get_hdraw(device)
 
     try:
         subprocess.run("git clone https://github.com/Mylab6/maschine.rs", shell=True, check=True)
     except:
         pass
     subprocess.run("cargo build --release", shell=True, check=True, cwd = get_local_file_path("maschine.rs") )
-    subprocess.run("cargo run --release /dev/hidraw3", shell=True, check=True, cwd = get_local_file_path("maschine.rs") )
+    subprocess.run("cargo run --release /dev/" + hdraw, shell=True, check=True, cwd = get_local_file_path("maschine.rs") )
 
         
 
