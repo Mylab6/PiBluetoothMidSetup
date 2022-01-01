30b9046ff096576a942f7c4a2023b4a8e5e7a18e
MyLab6 <asou1121@gmail.com>
2022-01-01 01:32:33 -0500

    aa

--- setup_maschine.py
+++ setup_maschine.py
@@ -4,7 +4,7 @@ import os
 import shutil
 
 def set_up_maschine():
-    results = subprocess.check_output("dmesg", shell=True)
+    results = subprocess.check_output("dmesg", universal_newlines=True).split('\n')
     for device in results:
         if "Native Instruments".lower() in device.lower():
             print(device)
