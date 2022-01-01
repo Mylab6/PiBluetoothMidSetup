f8880c7e9c4c1f92c0ece69c2bf4f72c94df4721
MyLab6 <asou1121@gmail.com>
2022-01-01 02:01:56 -0500

    S

--- setup_maschine.py
+++ setup_maschine.py
@@ -4,9 +4,20 @@ import os
 import shutil
 
 def set_up_maschine():
+    subprocess.run("curl https://sh.rustup.rs -sSf | bash -s -- -y")
+    subprocess.run("source $HOME/.cargo/env")
     results = subprocess.check_output("dmesg", universal_newlines=True).split('\n')
     for device in results:
         if "Native Instruments".lower() in device.lower():
             print(device)
+
+    try:
+        subprocess.run("git clone https://github.com/Mylab6/maschine.rs", shell=True, check=True)
+    except:
+        pass
+    subprocess.run("cargo build --release", shell=True, check=True, cwd = get_local_file_path("maschine.rs") )
+    subprocess.run("cargo run --release /dev/hidraw3", shell=True, check=True, cwd = get_local_file_path("maschine.rs") )
+
+        
 
 set_up_maschine()
